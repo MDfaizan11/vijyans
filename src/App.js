3d6876fe188e753cc83ceb89880cfaf723040aaf
MDfaizan11 <khanfaizan.qk@gmail.com>
2025-01-31 17:24:07 +0530

    name delete

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <ScrollToTop /> */}
+        <ScrollToTop />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
